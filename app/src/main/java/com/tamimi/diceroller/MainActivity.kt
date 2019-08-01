20a6772e0d10b62181c143802ba69d8d58258f2b
Yasser Tamimi <yosy2010@gmail.com>
2019-08-01 21:19:40 +0300

    final exercise

--- app/src/main/java/com/tamimi/diceroller/MainActivity.kt
+++ app/src/main/java/com/tamimi/diceroller/MainActivity.kt
@@ -2,7 +2,9 @@ package com.tamimi.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         rollBtn.text = "Let's Roll"
+        rollBtn.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice(){
+        val randomNum = (1..6).random()
+
+        // decide which image to show depending on the random number created
+        val diceResult = when(randomNum) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        // show the right image
+        diceImage.setImageResource(diceResult)
     }
 }
